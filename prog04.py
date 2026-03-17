e2a8cb7e6474cca0cf52409c1f0b5fb59ef95a14
anabautzer <ac.bautzer@hotmail.com>
2026-03-17 10:04:04 -0300

    Delete prog04.py

--- prog04.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system ("cls")
-
-# Fazer um algoritmo que calcule o dobro de um número dado pelo usuário
-
-num = int(input("Número: "))
-resul = (num + num)
-print(f"O dobro de {num} é {resul}")
